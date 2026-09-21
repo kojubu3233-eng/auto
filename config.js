fa0aaf2d5424794388332a4ad49f7b6bf7ff06c8
kojubu3233-eng <kojubu3233@gmail.com>
2026-09-21 14:27:09 +0900

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,12 @@ window.APP_CONFIG = {
   // 예정량/확정량 시트 = "자동화시스템"
   HISTORY_WEBAPP_URL: "https://script.google.com/macros/s/AKfycbw46wO5Yww9RavXDXaOcVCeysUkoEqTHkXGJCH5ozfsnRwJEIm9S4AXnrJPPP9QeXQQqQ/exec",
 
-  // 재고수불대장 (원래 잘 되던 URL로 복구)
+  // 재고수불대장
   LEDGER_WEBAPP_URL: "https://script.google.com/macros/s/AKfycbyYuCI_jY0S6yAFwxjZqmYjGmG7LXgalbedUqorFuL7GtadInkVl7NN9q7JjK7Vuec/exec",
 
+  // 확정량 저장 전용
   CONFIRM_WEBAPP_URL: "https://script.google.com/macros/s/AKfycbztWhg9G3OjM_kwg4CXSzPVZ-CKQy6kaZWVBHiuqXV8-9jvmxCpSYmNYsFUb9qoLp4RAA/exec",
 
-  OCR_WEBAPP_URL: "https://script.google.com/macros/s/AKfycbwF_ZqRdaRjpkpkkJOby33mAoQ1v4PsjHCd8s2j_Njy_hQWq_NjpHnQrsIKqLxqlq9z/exec"
+  // 거래명세서·전파문서 OCR 전용 (방금 새로 배포한 URL로 교체됨)
+  OCR_WEBAPP_URL: "https://script.google.com/macros/s/AKfycbyF--PABxdyVarU4JVNW4F-0rjFHskSTa0DsWVLlYAOhKXbfD-sOaOPEcjXv5dxrZZ1/exec"
 };
